aa224dd37f2506b713b070363075c506a61ddf59
Talar1997 <adam.talarczyk@hotmail.com>
2021-08-26 21:57:57 +0200

    Added endpoint for utilization data

--- src/controllers/utilizationController.js
+++ src/controllers/utilizationController.js
@@ -1,28 +1,7 @@
-// const base = require('./baseController')
-// const Utilization = require('./../models/utilization')
-const successResponse = require('./../controllers/response/successResponse')
-const {status} = require('../config/constants/statusCodes')
-const osu = require('node-os-utils')
+const {getAll, getOne} = require('./baseController')
+const Utilization = require("../models/utilization")
 
-//TODO: get data from database
 // eslint-disable-next-line no-unused-vars
-exports.getLast24hourData = async (req, res, next) => {
-    const usage = await osu.cpu.usage()
-    const memInfo = await osu.mem.info()
-    const os = await osu.os
-
-    const data = {
-        cpu: {
-            cpus: osu.cpu.count(),
-            usage: usage
-        },
-        memory: memInfo,
-        os: {
-            platform: os.platform(),
-            uptimie: os.uptime(),
-            ip: os.ip(),
-        }
-    }
-    successResponse(res, status.OK, data, true)
-
-}
+exports.getLast24hourData = getAll(Utilization)
+exports.getAll = getAll(Utilization)
+exports.getOne = getOne(Utilization)
